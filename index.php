74d7489a023063f7a7f4d3b74596ea50aa86f186
Eduardo Vizcaino Granados <e.vizcaino@solesgps.com>
2023-10-10 11:55:20 -0600

    Agregar archivos en contruccion

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+    echo "
+        <table width=\"100%\" height=\"100%\"  style=\"border:0;\">
+            <tr>
+                <td align=\"center\" valign=\"center\">
+                    <img src=\"logo designia.png\" style=\"border:0px;\">
+                </td>
+            </tr>
+        </table>
+    ";
+
+
+?>
